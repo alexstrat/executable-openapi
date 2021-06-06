62ea0a1f78bd50e3f872ef85c510b06abce83911
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2021-06-06 10:50:45 +0200

    small doc example

--- packages/executable-openapi-types/src/unicorn.ts
+++ packages/executable-openapi-types/src/unicorn.ts
@@ -1,4 +1,10 @@
+/**
+ * This class is a unicorn.
+ */
 export class Unicorn {
+  /**
+   * Make the unicor great someone.
+   */
   public sayHelloTo (name: string): string {
     return `🦄 Hello ${name} !`
   }
