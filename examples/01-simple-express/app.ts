ce90b562c5b24db431cff2d7a16a3fba6711eab7
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2022-01-14 17:33:16 +0100

    Make example use exectuable-openapi

--- examples/01-simple-express/app.ts
+++ examples/01-simple-express/app.ts
@@ -1,18 +1,19 @@
 import * as fs from 'fs'
 import * as express from 'express'
 import * as yaml from 'js-yaml'
-import { createRouter } from 'executable-openapi-router';
+import { createExecutableOpenAPI } from 'executable-openapi';
 import { OpenAPIObject } from 'openapi3-ts'
 import { executableOpenAPIExpressHandler } from 'executable-openapi-express-adapter';
 
-const spec = yaml.load(fs.readFileSync('./openapi.yaml', 'utf8')) as OpenAPIObject
+const document = yaml.load(fs.readFileSync('./openapi.yaml', 'utf8')) as OpenAPIObject
 const app = express()
 const port = 8080
 
-const execute = createRouter(spec, {
-  paths: {
-    '/hello/{name}': {
-      get: (parameters) => ({
+const execute = createExecutableOpenAPI({
+  document,
+  handlers: {
+    operations: {
+      getHello: (parameters) => ({
         status: 200,
         content: {
           'application/json': {
